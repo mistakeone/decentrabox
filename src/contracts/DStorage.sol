f91ab3f8a4a248854b0824fab3e5125875cff9dc
mistakeone <40323011+mistakeone@users.noreply.github.com>
2020-11-17 05:36:08 +0300

    created basic file upload functions added event for file uploading

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -2,19 +2,52 @@ pragma solidity ^0.5.0;
 
 contract DStorage {
   // Name
-  // Number of files
-  // Mapping fileId=>Struct 
-///
-  // Struct
+  string public name = 'DStorage';
+  uint public fileCount = 0;
+  mapping(uint => File) public files;
 
+  struct File {
+    uint fileId;
+    string fileHash;
+    uint fileSize;
+    string fileType;
+    string fileName;
+    string fileDescription;
+    uint uploadTime;
+    address payable uploader;
+  }
 
-  // Event
-
+  event FileUploaded(
+    
+    uint fileId,
+    string fileHash,
+    uint fileSize,
+    string fileType,
+    string fileName,
+    string fileDescription,
+    uint uploadTime,
+    address payable uploader
+  );
   constructor() public {
   }
 
   // Upload File function
+  function uploadFile(string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
+    require(bytes(_fileHash).length > 0);
+    require(bytes(_fileType).length > 0);
+    require(bytes(_fileDescription).length > 0);
+    require(bytes(_fileName).length > 0);
+    require(_fileSize > 0);
+    require(msg.sender!=address(0));
 
+
+    fileCount++;
+    files[fileCount] = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
+
+    emit FileUploaded(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
+
+
+  }
     // Make sure the file hash exists
 
     // Make sure file type exists
